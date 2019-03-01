561cc34478fb0797d0e6d77fe3692343e876bfd2
Glow <quiniasol@hotmail.com>
2019-03-01 02:55:00 +0100

    Last typos

--- linebreaks.py
+++ linebreaks.py
@@ -14,19 +14,19 @@ def finalMessage(text):
     print("----------------\n")
     print(text)
     print("\n----------------")
-    paste = input("Paste to clipboard? (Y/n): ")
+    paste = input("Paste to the clipboard? (Y/n): ")
     if (paste == "Y" or paste == "y"):
         pyperclip.copy(text)
-        print("Copied to clipboard succesfully.")
+        print("Copied to the clipboard succesfully.")
     else:
-        print("Not copied to clipboard.")
+        print("Not copied to the clipboard.")
 
 
 while True:
     print("\nLINE BREAKS FIXER")
     print("   1. Small breaks (two spaces)")
     print("   2. Big breaks (extra enter)")
-    mode = str(input("Input the mode you want to use: "))
+    mode = str(input("Please select the mode (1 or 2): "))
     text = pyperclip.paste()
     if (mode == "1"):
         text = smallBreaks(text)
